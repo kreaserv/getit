d3b894dba45ba5f00d7b011bb1572bc71b993f6e
nitesh <nj248591@gmail.com>
2015-07-20 12:44:54 +0530

    media query for tab

--- www/js/script.js
+++ www/js/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
 	
 	//var base_url = "http://192.168.1.106/getit_final/";
-	//var base_url = "http://localhost/getit_final/";
-	var base_url = "http://casaestilo.in/sumotech/getit/";
+	var base_url = "http://localhost/getit_final/";
+	//var base_url = "http://casaestilo.in/sumotech/getit/";
 	var pre_url = "";
 	
 	
